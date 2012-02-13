8566b725c1a847ebbed1adcb02e46ef02c64ad08
Xiaochun Shen <snowdonshen@gmail.com>
2012-02-13 21:29:53 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp321::Application.config.secret_token = '8720a242f9592d0380e6ee495f6d1255db161769f284262cd3cba7782bbe1354c41ae104caa30e9cccf50bfe84b2673e823c35ecf8e56e3864ac6aaed8eae408'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FirstApp321::Application.config.session_store :cookie_store, key: '_first_app321_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FirstApp321::Application.config.session_store :active_record_store
